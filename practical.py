75210fb8844fbed3fa643297d5d425245b1ae593
sree0715 <125061287+sree0715@users.noreply.github.com>
2023-03-05 22:01:23 +0530

    Add files via upload

---
+++ practical.py
@@ -0,0 +1,89 @@
+# x=1/2+3//3+4**2
+# print(x)
+
+# x= x%y  
+# y= y%x
+# print(y)
+
+# x=int(input("enter a value"))
+# y=int(input("enter a value"))
+# x = x%y
+# y = y%x
+# print(y)
+
+# x=1/1
+# print(x)
+
+# x=1//2*3
+# print(x)
+
+# x=input()
+# y=int(input())
+# print(x* y)
+
+# x=8%3
+# print(x)
+
+
+
+# t = [[
+
+# t=[[3-i for i in range(3)] for j in range(3)]
+# s=0
+# for i in range(3):
+#     s += t[i][i]
+#     print(s)
+
+# vals=[0,1,2]
+# vals.insert(0,1)
+# del vals[1]
+# print(vals)
+
+# x=[1,2,3]
+# for v in range(len(x)):
+#  x.insert(1,x[v])
+#  print(x)
+
+
+# for i in range(1):
+#     print("#")
+# else:
+#     print("#") 
+
+# x=[1,2,3,4]
+# print(x[-3:-2])
+
+
+# var=1
+# while var<10:
+#     print('#')
+#     var=var<<1
+
+# x=[1,2,3]
+# y=[1]
+# for v in x:
+#     y.insert(0,v)
+#     print(y)
+
+# i=0
+# while i <=3:
+#     i+=2
+#     print('*')
+
+# vals=[0,1,2]
+# vals[0].vals[2]=vals[2],vals[0]     #attribute error
+
+
+# a=1
+# b=0
+# c=a&b
+# d=a|b
+# e=a^b
+# print(c+d+e)
+
+# var=0
+# while var < 6:
+#     var+=1
+#     if var%2==0:
+#         continue
+#     print('#')
